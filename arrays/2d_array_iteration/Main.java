68194361575a0138c04bedb49d0b86235512244a
ninafilipovic10-afk <>
2025-11-19 21:53:33 +0000

    2D Array Iteration

---
+++ arrays/2d_array_iteration/Main.java
@@ -0,0 +1,35 @@
+package com.example;
+
+    public class Main {
+        public static double[] calculateDailyAverages(int[][] temperatures) {
+	        int days = temperatures[0].length;
+	        int stations = temperatures.length;
+            double[] dailyAverages = new double[days];
+
+            for (int day = 0; day < days; day++) {
+                int totalTemp = 0;
+                
+                
+                for (int stats = 0; stats < stations; stats++) {
+                    totalTemp += temperatures[stats][day];
+                }
+                dailyAverages[day] = (double) totalTemp/ stations;
+            }
+
+            return dailyAverages;
+        }
+
+        public static void main(String[] args) {
+            int[][] temperatures = {
+                    {20, 22, 24, 19, 21, 23, 25},
+                    {18, 20, 22, 19, 20, 22, 24},
+                    {21, 23, 25, 22, 24, 26, 28},
+                    {19, 21, 23, 20, 22, 24, 26}
+            };
+
+            double[] averages = calculateDailyAverages(temperatures);
+            for (int day = 0; day < averages.length; day++) {
+                System.out.println("Average temperature on day " + (day + 1) + ": " + averages[day]);
+            }
+        }
+    }
